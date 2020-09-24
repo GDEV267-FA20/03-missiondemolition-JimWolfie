08b6b514c30ab4ea407834081dd027d5ca08e1be
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-23 23:22:37 -0400

    bedtime
    i need to go to bed. working on clouds and castles tomorrow

--- Assets/Scripts/FollowScript.cs
+++ Assets/Scripts/FollowScript.cs
@@ -6,6 +6,11 @@ public class FollowScript : MonoBehaviour
 {
     static public GameObject POI;
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+
     [Header("Set dynamically")]
     public float camZ;
 
@@ -18,8 +23,13 @@ public class FollowScript : MonoBehaviour
         if(POI==null)return;
 
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+        
+        Camera.main.orthographicSize = destination.y +10;
     }
 
     // Update is called once per frame

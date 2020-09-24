7f376be99e694058b14f6f8c7e24a7e95d4a3a90
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-23 21:48:08 -0400

    k

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
-    //will switch to new input system when I have time. 
+    //will switch to new input system when I have time.
+    [Header("Set in Inpsector")]
+    public GameObject prefabProjectile;
+
+
+    [Header("Set Dynamically")] 
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimMode;
 
 
     void Awake()
@@ -13,6 +21,7 @@ public class Slingshot : MonoBehaviour
         Transform launchPointTransform = transform.Find("Launch");
         launchPoint = launchPointTransform.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTransform.position;
     }
     void OnMouseEnter()
     {
@@ -21,5 +30,12 @@ public class Slingshot : MonoBehaviour
     void OnMouseExit()
     {
         launchPoint.SetActive(false);
+    }
+    private void OnMouseDown()
+    {
+        aimMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic=true;
     }
 }

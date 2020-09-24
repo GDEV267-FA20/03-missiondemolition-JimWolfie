13d4cdd7cac85eea155b195b0aae69c1c4f2b32b
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-23 20:22:36 -0400

    halo and mouse over script
    mouse over is a part of unity engine not input!

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    //will switch to new input system when I have time. 
+
+   
+   
+    private void OnMouseEnter()
+    {
+
+    }
+    private void OnMouseExit()
+    {
+        
+    }
+}

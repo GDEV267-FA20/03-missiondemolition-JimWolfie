7fbcf3fef510bc3614e4bf1e8ef39df07465048c
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-24 20:48:55 -0400

    cloud generation made

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -49,10 +49,11 @@ public class Cloud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if(Input.GetKeyDown(KeyCode.Space))
         {
             Restart();
-        }
+        }*/
     }
     void Restart()
     {
